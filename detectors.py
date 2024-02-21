a691f2a5fdde0a677f4d1e90a3a3fed8ba365abd
Robert McPherson <robertmcpherson@Roberts-MacBook-Pro.local>
2024-02-21 18:06:26 -0500

    contours

--- detectors.py
+++ detectors.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import math 
+import graph
 
 # Load the precomputed Haar cascade classifiers
 face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
@@ -12,11 +13,9 @@ curr_eyes = [0,0,0,0]
 
 # Set up the SimpleBlobDetector with default parameters.
 params = cv2.SimpleBlobDetector_Params()
-# params.minThreshold = 10
-# params.maxThreshold = 200
-params.filterByArea = True
-params.minArea = 100  # min size for pupil area
-params.maxArea = 1000  # max size for pupil area
+# params.filterByArea = True
+# params.minArea = 100  # min size for pupil area
+# params.maxArea = 1000  # max size for pupil area
 # params.filterByCircularity = True
 # params.minCircularity = 0.2
 # params.filterByConvexity = True
@@ -24,6 +23,10 @@ params.maxArea = 1000  # max size for pupil area
 # params.filterByInertia = True
 # params.minInertiaRatio = 0.2
 detector = cv2.SimpleBlobDetector_create(params)
+
+# frame capture
+caps = []
+
 
 def detect_face(frame):
     # Detect faces in the frame
@@ -66,48 +69,97 @@ def resize_img(img, scale_factor = 1):
     height = int(480)
     return cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)   
 
-def get_keypoints(img, threshold=60, prev_area=0):
+def get_keypoints(img, threshold=60, prev_area=0, capture=False):
     if img is None or img.size == 0:
         return
 
     # Convert the image to grayscale
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred_img = cv2.GaussianBlur(gray_img, (15,15), 0)
+    equalized_img = cv2.equalizeHist(blurred_img)
+    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    # clahe_img = clahe.apply(equalized_img)
+
+    cv2.imshow("Equalized", resize_img(gray_img))
+
     # Convert image to binary
-    _, thresholded_image = cv2.threshold(gray_img, threshold, 255, cv2.THRESH_BINARY)
-  
+    # _, thresholded_image = cv2.threshold(gray_img, threshold, 255, cv2.THRESH_BINARY)
+    thresholded_image = cv2.adaptiveThreshold(equalized_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 2)
+
+    # morphing
+    # kernel = np.ones((5,5),np.uint8)
+    # thresholded_image = cv2.morphologyEx(thresholded_image, cv2.MORPH_OPEN, kernel)
+    
+    # blob_detect(thresholded_image)
+    contour_detect(thresholded_image)
+
+    if capture:         
+        if keypoints:
+            diameter = keypoints[0].size
+            radius = diameter / 2.0
+            area = 3.1415 * (radius ** 2)
+            caps.append([area, radius, diameter])
+            # print(f"Pupil diameter: {diameter} pixels")
+            print(f"Pupil area: {area} pixels")
+    else:
+        if len(caps) > 0:
+            # print average area of captured pupils
+            print(f"Average area: {sum([cap[0] for cap in caps]) / len(caps)}")
+            print(f"Average radius: {sum([cap[1] for cap in caps]) / len(caps)}")
+            print(f"Average diameter: {sum([cap[2] for cap in caps]) / len(caps)}")
+            graph.graph_pupils(caps, diameter=True, area=True)
+
+            caps.clear()
+
+
+def blob_detect(thresholded_image):
     keypoints = detector.detect(thresholded_image)
 
-    # Draw detected blobs as red circles.
     # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
     bin_im_with_keypoints = cv2.drawKeypoints(thresholded_image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    for i, keypoint in enumerate(keypoints):
-        # Extract the position of the keypoint
-        x, y = int(keypoint.pt[0]), int(keypoint.pt[1])
-        # Label the keypoint
-        diameter = keypoint.size
-        radius = diameter / 2.0
-        area = math.pi * (radius ** 2)
+    # for i, keypoint in enumerate(keypoints):
+    #     # Extract the position of the keypoint
+    #     x, y = int(keypoint.pt[0]), int(keypoint.pt[1])
+    #     # Label the keypoint
+    #     diameter = keypoint.size
+    #     radius = diameter / 2.0
+    #     area = math.pi * (radius ** 2)
         
-        label = f"A: {area:.0f}"
-        # draw line for diameter of pupil
-        cv2.line(im_with_keypoints, (x, y), (x + int(radius), y), (0, 255, 0), 2)
-        
-        cv2.putText(bin_im_with_keypoints, label, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+    #     label = f"D: {diameter:.0f}"
+    #     # label = f" A: {area:.0f}"
+    #     # draw line for diameter of pupil
+    #     # cv2.line(bin_im_with_keypoints, (x, y), (x + int(radius), y), (0, 255, 0), 2)
 
-    # Show keypoints
     cv2.imshow("Binary", resize_img(bin_im_with_keypoints))
-    # cv2.imshow("Keypoints", resize_img(im_with_keypoints))
+ 
+def contour_detect(thresholded_image):
+    # Find contours
+    contours, hierarchy = cv2.findContours(thresholded_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Assuming the pupil is the largest blob detected
-    # Calculate dimensions from the size attribute
-    if keypoints:
-        diameter = keypoints[0].size
-        radius = diameter / 2.0
-        area = 3.1415 * (radius ** 2)
+    print(f"Number of contours: {len(contours)}")
+    # Filter contours
+    # pupil_contours = []
+    # for contour in contours:
+    #     area = cv2.contourArea(contour)
+    #     # Assuming that the pupil will have a significant area,
+    #     # you can adjust the threshold according to your image
+    #     if area > 50:
+    #         perimeter = cv2.arcLength(contour, True)
+    #         # Check if contour is circular
+    #         if perimeter == 0: continue
+    #         circularity = 4 * np.pi * (area / (perimeter * perimeter))
+    #         # This threshold can be adjusted based on how circular you expect the pupil to be
+    #         if circularity > 0.7:
+    #             pupil_contours.append(contour)
 
-        # Output the dimensions of the pupil
-        print(f"Pupil diameter: {diameter} pixels")
-        print(f"Pupil radius: {radius} pixels")
-        print(f"Pupil area: {area} square pixels")
+    # # Assuming the largest circular contour is the pupil
+    # pupil_contours.sort(key=cv2.contourArea, reverse=True)
+    # if len(pupil_contours) == 0:
+    #     return
+    # pupil = pupil_contours[0]
+
+    # Draw the contour on the original image
+    # cv2.drawContours(thresholded_image, [pupil], -1, (255, 0, 0), 2)
+
+    cv2.imshow("Binary", resize_img(thresholded_image))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import detectors
+import time
 
 
 # cap = cv2.VideoCapture("eye_recording.flv")
@@ -10,6 +11,12 @@ roi_x, roi_y, roi_width, roi_height = 0, 0, 1080, 1080
 
 intensity = 20
 
+# timer variables
+capture = False
+start_time = time.time()
+elapsed_seconds = 0
+
+# initiate reocording interface
 
 while True:
     ret, frame = cap.read()
@@ -22,8 +29,10 @@ while True:
         break
     elif key == 0:  # Up arrow key
         intensity += 1
-    elif key == 1:  # Down arrow key
-        intensity -= 1
+    elif key == 32:  # Down arrow key
+        capture = not capture
+
+
 
     # Ensure ROI dimensions are within frame boundaries
     roi_height = max(1, min(roi_height, frame.shape[0] - roi_y))
@@ -34,7 +43,7 @@ while True:
     rows, cols, _ = roi.shape
 
     # Create reticle in the center of the ROI
-    x = int(cols/2) + 50
+    x = int(cols/2) + 400
     y = int(rows/2)
     width = 100
     # cv2.rectangle(roi, (x,y), (x+100, y+100) (0, 0, 255), 2)
@@ -44,12 +53,20 @@ while True:
     # Detect eyes in the ROI
     # eye_roi = detectors.detect_features(roi, reticle_roi)
 
+    # Detect pupils in the eye ROI
+    detectors.get_keypoints(reticle_roi, capture=capture)
+
     # Display the ROI
     cv2.imshow("ROI", roi)
 
-    # Detect pupils in the eye ROI
-    detectors.get_keypoints(reticle_roi)
     
+    
+    current_time = time.time()
+    if current_time - start_time >= 1:
+        elapsed_seconds += 1
+        # print(f"Elapsed seconds: {elapsed_seconds}")
+        start_time = current_time
+
 
 cv2.destroyAllWindows()
 
